e553ff41a483b6a248199952d31b6dd693a4d2ec
l33th <l33th@protonmail.ch>
2022-01-01 07:18:39 +0200

    signed integer

--- 7-DataTypes/DataTypes.sol
+++ 7-DataTypes/DataTypes.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.11;
 contract AbstractContract {
     string public stringValue = "l33thContract";
     bool public myBool = true;
-    int public myInt = 1;
-    uint public muUint = 7;
+    int public mySignedInt = -1;
+    int public myInt = 1
+    uint public myUint = 7;
 }
