f8b28fbfa29d86f3f05df3aca8ba144842abf479
l33th <l33th@protonmail.ch>
2022-01-05 08:47:36 +0200

    custom modifier - check if sender is owner

---
+++ 12-CustomModifier/CustomModifier.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.10;
+
+contract L33thContract {
+    uint256 public peopleCount = 0;
+    mapping(uint256 => Person) public people;
+
+    address owner;
+
+    modifier onlyOwner() {
+        msg.sender == owner
+    }
+
+    struct Person {
+        uint256 _id;
+        string _firstName;
+        string _lastName;
+    }
+
+    function addPerson(string memory _firstName, string memory _lastName)
+        public
+        onlyOwner
+    {
+        incrementCount();
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+
+    function incrementCount() internal {
+        peopleCount += 1;
+    }
+}
