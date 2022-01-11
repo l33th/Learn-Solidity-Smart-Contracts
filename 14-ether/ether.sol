7517192231a71faddee1f6094062990f9fcde001
l33th <l33th@protonmail.ch>
2022-01-11 07:12:39 +0200

    wallet transfer setup

--- 14-ether/ether.sol
+++ 14-ether/ether.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.8.10;
 
 contract EthContract() {
     mapping(address => uint256) public balances;
+    address wallet;
 
     function buyToken() {
         // buy a token
         balances[msg.sender] += 1;
+        wallet.transfer();
         // send ether to the wallet
     }
 }
