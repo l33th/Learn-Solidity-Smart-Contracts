723dae71759b495d079afc2f0ab52839990b32e8
l33th <l33th@protonmail.ch>
2022-01-11 07:26:09 +0200

    added an event

---
+++ 15-Events/Event.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.10;
+
+contract EthContract() {
+    mapping(address => uint256) public balances;
+    address payable wallet;
+
+    event Purchase(
+        address _buyer,
+        uint256 _amount
+    );
+ 
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+    }
+
+    function() external payable {
+        buyToken();
+    }
+
+    function buyToken() public payable {
+        // buy a token
+        balances[msg.sender] += 1;
+        // send ether to the wallet
+        wallet.transfer(msg.value);
+        emit Purchase(msg.sender, 1)
+    }
+}
