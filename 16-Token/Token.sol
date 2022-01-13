06a81c0c5602d0bc16ba13e006fcdbc013d565b3
l33th <l33th@protonmail.ch>
2022-01-13 06:48:37 +0200

    erc20 token cleanup

--- 16-Token/Token.sol
+++ 16-Token/Token.sol
@@ -3,16 +3,14 @@ pragma solidity ^0.8.10;
 contract ERC20Token {
     string public name;
     mapping(address => uint256) public balances;
+    function mint() public {
+        balances[msg.sender] ++;
+    }
 }
 
 contract EthContract() {
     mapping(address => uint256) public balances;
     address payable wallet;
-
-    event Purchase(
-        address indexed _buyer,
-        uint256 _amount
-    );
  
     constructor(address payable _wallet) public {
         wallet = _wallet;
@@ -23,10 +21,6 @@ contract EthContract() {
     }
 
     function buyToken() public payable {
-        // buy a token
-        balances[msg.sender] += 1;
-        // send ether to the wallet
         wallet.transfer(msg.value);
-        emit Purchase(msg.sender, 1)
     }
 }
