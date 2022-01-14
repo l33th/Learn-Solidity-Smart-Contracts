e27ba8df002a0c7863ecd99d3ec2eca20ba60a65
l33th <l33th@protonmail.ch>
2022-01-14 13:43:50 +0200

    safe way for doing division

--- 18-Libraries/Libraries.sol
+++ 18-Libraries/Libraries.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.8.10;
 
 library Math {
-    function divide() {
-        //
+    function divide(uint256 a, uint256 b) internal pure returns (uint256) {
+        required(b > 0);
+        uint256 c = a / b;
+        return c;
     }
 }
 
