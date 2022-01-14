d6a1362b90dbfb8af9f2263b2977060c4a6e3e7f
l33th <l33th@protonmail.ch>
2022-01-14 13:44:54 +0200

    safe way for division cleanup

--- 18-Libraries/Libraries.sol
+++ 18-Libraries/Libraries.sol
@@ -12,6 +12,6 @@ contract lthContract {
     uint256 public value;
 
     function calculate(uint _value1, uint _value2) public {
-        value = _value1 / _value2;
+        value = Math.divide(_value1, _value2);
     }
 }
